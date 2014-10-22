3341587463e7d7b0176fda72e3d0c855101c9804
Conor Kosidowski <ckosidowski11@winona.edu>
2014-10-22 17:08:23 -0500

    Equal Length Action Bar Buttons
    Update for Action Bar

--- App Files/src/com/upac/upacapp/MainActivity.java
+++ App Files/src/com/upac/upacapp/MainActivity.java
@@ -1,61 +1,63 @@
-    package com.upac.upacapp;
+package com.upac.upacapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 	
-	android.app.ActionBar ab;
+	private static int currFrag;
+	private static int nextFrag;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		android.app.ActionBar ab;
+		
+/*		Button b = (Button) findViewById(R.id.action_events_button);
+		b.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				currFrag = nextFrag;
+				nextFrag = R.layout.fragment_calendar;
+				getSupportFragmentManager().beginTransaction().replace(currFrag, new PlaceholderFragment())
+											.addToBackStack(null).commit();
+			}
+		});*/
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		if (savedInstanceState == null) {
+			nextFrag = R.layout.fragment_events;
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
 		ab = this.getActionBar();
+		ab.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM, ActionBar.DISPLAY_SHOW_CUSTOM);
+		ab.setCustomView(R.layout.ab_custom_view);
 		ab.setDisplayShowTitleEnabled(false);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
+	/*
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
 
-		public PlaceholderFragment() {
+		public PlaceholderFragment(){
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
+				Bundle savedInstanceState){
+			View rootView = inflater.inflate(nextFrag, container,
 					false);
 			return rootView;
 		}
